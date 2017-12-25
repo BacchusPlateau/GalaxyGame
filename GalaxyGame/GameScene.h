806f583f2f08272f3632335cc6548e5c64b1e739
Bret Williams <bret@brets-macbook-pro.local>
2017-12-25 11:58:33 -0600

    Initial Commit

---
+++ GalaxyGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  GalaxyGame
+//
+//  Created by Bret Williams on 12/25/17.
+//  Copyright © 2017 Bret Williams. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ GalaxyGame/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  GalaxyGame
+//
+//  Created by Bret Williams on 12/25/17.
+//  Copyright © 2017 Bret Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
